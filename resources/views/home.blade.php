f5dcfd58682ec37f4b70ffe4f86287784e563731
Tristan Jake Alcantara <alcantaratristanjake@gmail.com>
2022-12-07 02:27:55 +0000

    Section home page based on Figma sections.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,40 @@
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+
+{{-- NAVIGATION SECTION --}}
+
+
+{{-- BENEFITS SECTION --}}
+
+
+{{-- HERO SECTION --}}
+
+
+{{-- SITE-WIDE DEALS SECTION --}}
+
+
+{{-- VIP EXCLUSIVE SECTION --}}
+
+
+{{-- TRENDING SECTION --}}
+
+
+{{-- NEW IN SECTION --}}
+
+
+{{-- RECENTLY BOUGHT SECTION --}}
+
+
+{{-- INSTA SECTION --}}
+
+
+{{-- NEWSLETTER SECTION --}}
+
+
+{{-- FOOTER SECTION --}}
+
+
+
 </body>
 </html>
