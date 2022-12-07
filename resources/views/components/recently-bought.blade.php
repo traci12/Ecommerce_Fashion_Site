abac1cf799eaa853cf6735d4948fbd49fad2fa37
Tristan Jake Alcantara <alcantaratristanjake@gmail.com>
2022-12-07 18:06:58 +0000

    Update recently bought section on sales.

--- resources/views/components/recently-bought.blade.php
+++ resources/views/components/recently-bought.blade.php
@@ -18,31 +18,31 @@
     <div class="flex items-start flex-col">
       <image alt="Sale Gallery 1" src="{{ asset('/images/sale1.png') }}" />
       <p class="not-italic flex items-center capitalized text-black font-normal text-sm">Festive Looks Rust Red Ribbed Velvet Long Sleeve Bodysuit</p>
-      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm">$38</p>
+      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm grid-cols-2 gap-5"><span>$38</span></p>
     </div>
 
     <div class="flex items-start flex-col">
       <image alt="Sale Gallery 2" src="{{ asset('/images/sale2.png') }}" />
       <p class="not-italic flex items-center capitalized text-black font-normal text-sm">Chevron Flap Crossbody Bag</p>
-      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm">$5.77     $7.34</p>
+      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm grid-cols-2 gap-5"><span class="sale">$5.77</span><span class="line-through">$7.34</span></p>
     </div>
 
     <div class="flex items-start flex-col">
       <image alt="Sale Gallery 3" src="{{ asset('/images/sale3.png') }}" />
       <p class="not-italic flex items-center capitalized text-black font-normal text-sm">Manilla Tan Multi Plaid Oversized Fringe Scarf</p>
-      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm">$29    $39</p>
+      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm grid-cols-2 gap-5"><span class="sale">$29</span><span class="line-through">$39</span></p>
     </div>
 
     <div class="flex items-start flex-col">
       <image alt="Sale Gallery 4" src="{{ asset('/images/sale4.png') }}" />
       <p class="not-italic flex items-center capitalized text-black font-normal text-sm">Diamante Puff Sleeve Dress - Black</p>
-      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm">$49.99</p>
+      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm grid-cols-2 gap-5"><span>$49.99</span></p>
     </div>
 
     <div class="flex items-start flex-col">
       <image alt="Sale Gallery 5" src="{{ asset('/images/sale5.png') }}" />
       <p class="not-italic flex items-center capitalized text-black font-normal text-sm">Banneth Open Front Formal Dress in Black</p>
-      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm">$69    $99.95</p>
+      <p class="not-italic flex items-center capitalized text-black font-semibold text-sm grid-cols-2 gap-5"><span class="sale">$69</span><span class="line-through">$99.95</span></p>
     </div>
   </div>
 
