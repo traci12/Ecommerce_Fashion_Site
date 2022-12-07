c004a3d587ad15be87b48bfaa1c66800d9a12ad9
Tristan Jake Alcantara <alcantaratristanjake@gmail.com>
2022-12-07 15:51:04 +0000

    Update insta section. Fix responsiveness.

--- resources/views/components/insta.blade.php
+++ resources/views/components/insta.blade.php
@@ -16,11 +16,21 @@
   <p class="flex flex-row items-start justify-center font-normal text-sm text-center items-center not-italic">Checkout who’s wearing what by using #THREADEDInspo on Instagram</p>
 
   <div class="insta-gallery flex justify-center items-center grid-cols-5 gap-3">
-    <image alt="Insta Gallery 1" src="{{ asset('/images/insta1.png') }}" />
-    <image alt="Insta Gallery 2" src="{{ asset('/images/insta2.png') }}" />
-    <image alt="Insta Gallery 3" src="{{ asset('/images/insta3.png') }}" />
-    <image alt="Insta Gallery 4" src="{{ asset('/images/insta4.png') }}" />
-    <image alt="Insta Gallery 5" src="{{ asset('/images/insta5.png') }}" />
+    <div class="flex items-start flex-col">
+      <image alt="Insta Gallery 1" src="{{ asset('/images/insta1.png') }}" />
+    </div>
+    <div class="flex items-start flex-col">
+      <image alt="Insta Gallery 2" src="{{ asset('/images/insta2.png') }}" />
+    </div>
+    <div class="flex items-start flex-col">
+      <image alt="Insta Gallery 3" src="{{ asset('/images/insta3.png') }}" />
+    </div>
+    <div class="flex items-start flex-col">
+      <image alt="Insta Gallery 4" src="{{ asset('/images/insta4.png') }}" />
+    </div>
+    <div class="flex items-start flex-col">
+      <image alt="Insta Gallery 5" src="{{ asset('/images/insta5.png') }}" />
+    </div>
   </div>
 
   <div class="view-all-posts justify-center flex flex-row">
